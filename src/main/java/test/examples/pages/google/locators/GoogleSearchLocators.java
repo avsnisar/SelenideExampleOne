69ee1f1ab5213a3443662fdb91cc951fa4f43b35
avsnisar <avsnisar@ukr.net>
2023-10-17 00:40:56 +0300

    added dependency and updated tests

--- src/main/java/test/examples/pages/google/locators/GoogleSearchLocators.java
+++ src/main/java/test/examples/pages/google/locators/GoogleSearchLocators.java
@@ -3,7 +3,8 @@ package test.examples.pages.google.locators;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-
+// import static org.openqa.selenium.By.*;
+import static com.codeborne.selenide.Selectors.byCssSelector;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -11,6 +12,7 @@ public class GoogleSearchLocators {
 
     protected final SelenideElement inputSearch = $(byXpath("//textarea[@name='q']"));
     protected final SelenideElement selenium = $(By.cssSelector("//*//input[1]"));
+    protected final SelenideElement selenium2 = $(byCssSelector("//*//input[1]"));
     protected final SelenideElement xpathOne = $x("//*//textarea[1]");
     protected final ElementsCollection xpathMany = $$x("//*");
 }
